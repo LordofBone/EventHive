4563072123331453328d388e1a0dfc1fa82583b4
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-01-31 00:28:28 +0000

    Logging updated

--- template/producer.py
+++ template/producer.py
@@ -4,12 +4,16 @@ import time
 from event_hive_runner import EventActor
 from template.custom_events import TestEvent, OtherTestEvent, PingTestEvent
 
+logger = logging.getLogger(__name__)
+
 
 class Producer(EventActor):
     def __init__(self, event_queue):
         super().__init__(event_queue)
         self.max_loop = 10
         self.loop_count = 0
+
+        logger.debug(f"{self.__class__.__name__} Initialized")
 
     def get_event_handlers(self):
         # No event handlers for the Producer since it's not supposed to consume any events.
@@ -25,7 +29,7 @@ class Producer(EventActor):
         :return:
         """
         while self.loop_count < self.max_loop and self.is_running:
-            logging.debug(f"{self.__class__.__name__} Producing...")
+            logger.debug(f"{self.__class__.__name__} Producing...")
             if self.loop_count == 1:
                 event = OtherTestEvent(["TEST_CONTENT"], 2)
             elif self.loop_count == 4:
@@ -42,4 +46,4 @@ class Producer(EventActor):
             # simulate some delay, seems to be at least 2 seconds or the queue can't keep up (some sort of race
             # condition?) todo: investigate
             time.sleep(2)
-        logging.info(f"{self.__class__.__name__} Producer thread finished")
+        logger.info(f"{self.__class__.__name__} Producer thread finished")
--- template/runner.py
+++ template/runner.py
@@ -7,12 +7,14 @@ from template.consumer_producer import ConsumerProducer
 from template.consumer_producer_2 import ConsumerProducer2
 from template.producer import Producer
 
+logger = logging.getLogger(__name__)
+
 
 def main():
-    logging.debug("Starting event hive runner")
+    logger.debug("Starting event hive runner")
     event_queue = EventQueue()
 
-    logging.debug("Starting producer and consumer threads")
+    logger.debug("Starting producer and consumer threads")
     producer_process = Producer(event_queue)
     consumer_process = Consumer(event_queue)
     producer_consumer_process = ConsumerProducer(event_queue)
@@ -23,7 +25,7 @@ def main():
     producer_consumer_process.start()
     producer_consumer_process_2.start()
 
-    logging.debug("Waiting for threads to finish")
+    logger.debug("Waiting for threads to finish")
     producer_process.join()
     consumer_process.join()
     producer_consumer_process.join()
